5b110a887dae483398cce46b1cfcf8f884737ede
AaqibNabi <aaqib.shaavir@gmail.com>
2023-05-30 19:35:51 +0530

    registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  
 })
 export class RegisterComponent {
-
+name:string=""
+label:string="Register"
+username:string=""
+handleRegister(){
+  console.log(this.username)
+}
 }
