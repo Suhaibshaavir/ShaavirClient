b51798566b98573b41841adc5e71b0be3fd882e0
ASIFAHMADDAR <darasif.cse@gmail.com>
2023-06-01 20:37:57 +0530

    register_aqib

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
 
+interface IRegisterProps{
+  fullName:string,
+  email:string,
+  contact:number,
+  fileData:string,
+  password:string,
+  gender:string
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-
 })
 export class RegisterComponent {
 show:boolean=false;
 show1:boolean=false
-name:string=""
-label:string="Register"
-username:string=""
+formValue:IRegisterProps = <IRegisterProps> {}
+cPassword:string=''
+
 handleRegister(){
-  console.log(this.username)
+ console.log(this.formValue);
+ 
 }
 
 passwordToggle() {
